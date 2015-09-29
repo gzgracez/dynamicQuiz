1559de9ce3705b37206f3156e66ea96331f214f3
Grace Zhang <gz.gracez16@gmail.com>
2015-09-29 16:02:51 -0400

    dq

---
+++ static/js/quiz.js
@@ -0,0 +1,22 @@
+var quiz = { 
+	"title": "Physics Quiz",
+	"description": ".....",
+	"meta_tags": [
+		"sceince","physics", "fun"
+	],
+	"difficulty": 1-20,
+	"questions": [
+		{
+			"text": "What is the secret number?",
+			"answers": ["0", "42", "17"],
+			"correct_answer": 1,
+			"meta_tags": ["geeky humor"]
+		},
+		{
+			"text": "Question 2?",
+			"answers": ["0", "42", "17"],
+			"correct_answer": 1,
+			"meta_tags": ["geeky humor"]
+		}
+	]
+}
--- static/js/script.js
+++ static/js/script.js
@@ -1,34 +1,20 @@
 var currentQuestion = 0;
 
-var quiz = { 
-	"title": "Physics Quiz",
-	"description": ".....",
-	"meta_tags": [
-		"sceince","physics", "fun"
-	],
-	"difficulty": 1-20,
-	"questions": [
-		{
-			"text": "What is the secret number?",
-			"answers": ["0", "42", "17"],
-			"correct_answer": 1,
-			"meta_tags": ["geeky humor"]
-		},
-		{
-			"text": "Question 2?",
-			"answers": ["0", "42", "17"],
-			"correct_answer": 1,
-			"meta_tags": ["geeky humor"]
-		}
-	]
-}
-
 document.getElementById("nextQuestion").addEventListener("click", nextQuestion);
+// document.getElementById("nameForm").addEventListener("submit", test);
 
 function nextQuestion() {
-	// if currentQuestion<quiz["questions"].length {
-	// 	currentQuestion+=1;
-	// }
+	while (currentQuestion<quiz["questions"].length) {
+		currentQuestion+=1;
+	}
 	console.log(currentQuestion);
 	document.getElementById("question").innerHTML = quiz["questions"][currentQuestion]["text"];
+}
+
+function test(){
+	console.log(document.getElementById("firstname").value);
+	var name = document.getElementById("firstname").value;
+
+
+	document.getElementById("question").innerHTML = name;
 }

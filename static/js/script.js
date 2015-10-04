8899f7efadffc57816a627f53f8ec61f5a10fa4e
Grace Z <gz.gracez16@gmail.com>
2015-10-04 00:56:14 -0400

    final screen

--- static/js/script.js
+++ static/js/script.js
@@ -1,24 +1,25 @@
+var name = "Name";
 var currentQuestion = -1;
 var quizLength = 0;
 
 // Initial setup
 $(document).ready(function() {
-  quizLength = Math.ceil(Math.random()*(quiz["questions"].length-(quiz["questions"].length/2)))+(quiz["questions"].length/2)
-  $('#title').text(quiz["title"]);
-  $('#title').text("Grace's Dynamic Quiz");
-  $('#answerChoices').hide();
-  $('#nextQuestion').hide();
-  $('#nameForm').submit(function (e) {
-    e.preventDefault();
-    nameForm();
+  // quizLength = Math.ceil(Math.random()*(quiz["questions"].length-(quiz["questions"].length/2)))+(quiz["questions"].length/2)
+  quizLength = Math.ceil(Math.random()*(quiz["questions"].length-(quiz["questions"].length/2))+(quiz["questions"].length/2));
+    $('#title').text(quiz["title"]);
+    $('#title').text("Grace's Dynamic Quiz");
+    $('#answerChoices').hide();
+    $('#nextQuestion').hide();
+    $('#nameForm').submit(function (e) {
+      e.preventDefault();
+      nameForm();
+    });
+    document.getElementById("nextQuestion").addEventListener("click", nextQuestion);
   });
-  document.getElementById("nextQuestion").addEventListener("click", nextQuestion);
-});
 
 // After name is submitted on initial screen
 function nameForm(){
-  var nameArray = $('#nameForm').serializeArray();
-  var name = nameArray[0]["value"];
+  name = $('#nameForm').serializeArray()[0]["value"];
   $('#nameForm').hide();
   $('#nextQuestion').show();
   $('#answerChoices').show();
@@ -30,24 +31,35 @@ function nameForm(){
 function nextQuestion() {
 	if (currentQuestion<quizLength-1) {
 		currentQuestion+=1;
-	}
-	console.log(quizLength);
-  $('#questionNumber').text("Question " + (currentQuestion+1));
-	$('#question').text(quiz["questions"][currentQuestion]["text"]);
-  var numAns = 2 + Math.ceil((Math.random()*3));
-  console.log(numAns);
-  for (var i = 0; i<numAns; i++) {
-    $('#' + i).show();
-    var aID = "label[for=" + i + "]";
-    $(aID).show();
-    console.log(aID);
-    console.log(quiz["questions"][currentQuestion]["answers"][i]);
-    $(aID).html(quiz["questions"][currentQuestion]["answers"][i]);
+    console.log(quizLength);
+    $('#questionNumber').text("Question " + (currentQuestion+1));
+    $('#question').text(quiz["questions"][currentQuestion]["text"]);
+    var numAns = 2 + Math.ceil((Math.random()*3));
+    console.log(numAns);
+    for (var i = 0; i<numAns; i++) {
+      $('#' + i).show();
+      // uncheck answers
+
+      var aID = "label[for=" + i + "]";
+      $(aID).show();
+      console.log(aID);
+      console.log(quiz["questions"][currentQuestion]["answers"][i]);
+      $(aID).html(quiz["questions"][currentQuestion]["answers"][i]);
+    }
+    for (var i = numAns; i<5; i++) {
+      console.log("hide");
+      $('#' + i).hide();
+      var labelID = "label[for=" + i + "]";
+      $(labelID).hide();
+    }
   }
-  for (var i = numAns; i<5; i++) {
-    console.log("hide");
-    $('#' + i).hide();
-    var labelID = "label[for=" + i + "]";
-    $(labelID).hide();
+  else {
+    $('#welcome').hide();
+    $('#questionNumber').hide();
+    $('#question').hide();
+    $('#nextQuestion').hide();
+    $('#answerChoices').hide();
+    $('#score').show();
+    $('#nameScore').text(name + ", your score on this quiz is:");
   }
 }

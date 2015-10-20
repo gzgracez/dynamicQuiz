3aa2f9356873b19c8d46f2a939b968b50295ee09
Grace Zhang <gz.gracez16@gmail.com>
2015-10-20 14:12:59 -0400

    working static folder

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var app = express();
-app.use("/public", express.static(path.join(__dirname, 'public')));
+var path = require('path');
+app.use("/static", express.static(path.join(__dirname, 'static')));
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  // res.send('Hello World!');
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	var page = '<html class="no-js" lang="en" ><head> <meta charset="utf-8"> <meta name="viewport" content="width=device-width, initial-scale=1.0"> <title>Dynamic Quiz</title> <link rel="stylesheet" href="static/css/bootstrap.min.css"> <link rel="stylesheet" href="static/css/styles2.css"> <link rel="shortcut icon" href="static/1.ico"/></head><body> <div class="container"> <h1 id="title"></h1> <div class="alert alert-danger" id="nameFormWarning" role="alert"> <p class="nameAlert">Please enter your name before proceeding!&nbsp;<a href="#" class="close nameAlert" data-hide="alert">&times;</a></p></div><form id="nameForm" role="form"> <div class="form-group"> <label for="name">First Name:</label> <input type="text" name="nameValue" placeholder="First Name" class="form-control"><br><input class="btn btn-primary" class="form-control" type="submit" value="Submit"> </div></form> <h2 id="welcome"></h2> <div class="alert alert-danger" id="answerWarning" role="alert"> <p class="nameAlert">Please choose an answer before proceeding!&nbsp;<a href="#" class="close nameAlert" data-hide="alert">&times;</a></p></div><h3 id="questionNumber"></h3> <h3 id="question"></h3> <form id="answerChoices"> <input type="radio" name="answers" id="0"> <label for="0" id="ansLabels"></label><br><input type="radio" name="answers" id="1"> <label for="1" id="ansLabels"></label><br><input type="radio" name="answers" id="2"> <label for="2" id="ansLabels"></label><br><input type="radio" name="answers" id="3"> <label for="3" id="ansLabels"></label><br><input type="radio" name="answers" id="4"> <label for="4" id="ansLabels"></label> </form> <button id="previousQuestion" class="btn btn-danger btn-sm"><< Back</button> <button id="nextQuestion" class="btn btn-success btn-sm">Next >></button> <h2 id="nameScore"></h2> <center> <canvas id="piechart" width="480" height="250"></canvas> </center> <table class="table table-hover" id="scoreTable"> <thead> <tr> <th>Question</th> <th>Your Answer</th> <th>Correct Answer</th> </tr><tbody> </tbody> </thead> </table> <center> <a href="" id="home" class="btn btn-primary btn-sm">Back To Home</a> </center> <br></div><script src="static/js/jquery-2.1.4.min.js"></script><script src="static/js/bootstrap.min.js"></script><script src="static/js/quiz.js"></script><script src="static/js/script.js"></script></body></html>';
+  res.write(page);
+	res.end();
 });
 
 var server = app.listen(3000, function () {
---
+++ static/js/script.js
@@ -0,0 +1,279 @@
+// Make separate method for checking which answer is selected
+
+var name = "Name";
+var currentQuestion = -1;
+var quizLength = 0;
+var numAns = 0;
+var userAnswers = [];
+var score = 0;
+
+// Initial setup
+$(document).ready(function() {
+  // quizLength = Math.ceil(Math.random()*(quiz["questions"].length-(quiz["questions"].length/2))+(quiz["questions"].length/2));
+  quizLength = quiz["questions"].length;
+  console.log(quizLength);
+  $('#title').text(quiz["title"]);
+  $('#title').hide().fadeIn("slow");
+  $('#nameForm').hide().fadeIn("slow");
+  $('#answerChoices').hide();
+  $('#previousQuestion').hide();
+  $('#nextQuestion').hide();
+  $('#answerWarning').hide();
+  $('#nameFormWarning').hide();
+  $('#scoreTable').hide();
+  $('#home').hide();
+  $('[data-hide]').on("click", function(){
+    $('#nameFormWarning').hide();
+    $('#answerWarning').hide();
+  });
+  $('#nameForm').submit(function (e) {
+    e.preventDefault();
+    nameForm();
+  });
+  $('#piechart').hide();
+  document.getElementById("previousQuestion").addEventListener("click", back);
+  document.getElementById("nextQuestion").addEventListener("click", nextQuestion);
+});
+
+// After name is submitted on initial screen
+function nameForm(){
+  name = $('#nameForm').serializeArray()[0]["value"];
+  if (name.length === 0) {
+    $('#nameFormWarning').fadeIn();
+  }
+  else {
+    $('#nameFormWarning').hide();
+    $('#nameForm').hide();
+    $('#nextQuestion').show();
+    $('#answerChoices').show();
+    $('#welcome').text("Welcome " + name + "!");
+    nextQuestion();
+  }
+}
+
+// Show questions and answers
+function generateQA (){
+  console.log(quizLength);
+  $('#questionNumber').text("Question " + (currentQuestion+1)).hide().fadeIn();
+  $('#question').text(quiz["questions"][currentQuestion]["text"]).hide().fadeIn();
+  numAns = quiz["questions"][currentQuestion]["answers"].length;
+  // if answered already
+  if (currentQuestion < userAnswers.length) {
+    $('input[name="answers"][id="' + userAnswers[currentQuestion][2] + '"]').prop('checked',true);
+    for (var i = 0; i < numAns; i++) {
+      $('#' + i).fadeIn();
+      // answer choices radio button labels
+      var aID = "label[for=" + i + "]";
+      $(aID).fadeIn();
+      $(aID).html(quiz["questions"][currentQuestion]["answers"][i]).hide().fadeIn();
+    }
+    // hide excess answer choices
+    for (var a = numAns; a<5; a++) {
+      $('#' + a).fadeOut();
+      var labelID = "label[for=" + a + "]";
+      $(labelID).fadeOut();
+    }
+
+  }
+  // if hasn't been answered before
+  else {
+    // uncheck answers
+    $('input[name="answers"]').prop('checked',false);
+    for (var i = 0; i < numAns; i++) {
+      $('#' + i).fadeIn();
+      // answer choices radio button labels
+      var aID = "label[for=" + i + "]";
+      $(aID).fadeIn();
+      $(aID).html(quiz["questions"][currentQuestion]["answers"][i]).hide().fadeIn();
+    }
+    // hide excess answer choices
+    for (var a = numAns; a<5; a++) {
+      $('#' + a).fadeOut();
+      var labelID = "label[for=" + a + "]";
+      $(labelID).fadeOut();
+    }
+
+  }
+  if (currentQuestion === 0) {
+    $('#previousQuestion').hide();
+  }
+  else {
+    $('#previousQuestion').show();
+  }
+}
+
+function back(){
+  if (currentQuestion <= 0) {
+    $('#previousQuestion').hide();
+  }
+  else {
+    currentQuestion--;
+    if (currentQuestion <= 0) {
+      $('#previousQuestion').hide();
+    }
+    generateQA();
+    var pCheckedAnswer = userAnswers[currentQuestion][2];
+    $('input[name="answers"][id="' + pCheckedAnswer + '"').prop('checked',true);
+  }
+}
+
+// Check which radio button is checked and record
+function whichChecked() {
+  for (var i = 0; i < numAns; i++) {
+    if ($("input[name='answers'][id='" + i + "']").is(':checked')) {
+      console.log ("The answer chosen is " + i + " " + quiz["questions"][currentQuestion]["correct_answer"]);
+      // if already added to userAnswers
+      if (currentQuestion < userAnswers.length) {
+        if (i === quiz["questions"][currentQuestion]["correct_answer"]) {
+          userAnswers[currentQuestion][1] = true;
+          userAnswers[currentQuestion][2] = i;
+        }
+        else {
+          userAnswers[currentQuestion][1] = false;
+          userAnswers[currentQuestion][2] = i;
+        }
+      }
+      // if new answer
+      else {
+        if (i === quiz["questions"][currentQuestion]["correct_answer"])
+          userAnswers.push([currentQuestion, true, i]);
+        else
+          userAnswers.push([currentQuestion, false, i]);
+      }
+    }
+  }
+}
+
+// Calculate Score
+function calculateScore() {
+  for (var i = 0; i < quizLength; i++){
+    if (userAnswers[i][1]) {
+      score++;
+    }
+  }
+  console.log(score);
+}
+
+// Display score table
+function scorePerQuestionTable() {
+  for (var r = 0; r < quizLength; r++) {
+    $('#scoreTable').fadeIn("slow");
+    if (userAnswers[r][1])
+      $('#scoreTable > tbody:last-child').append('<tr class="success"><td class="questionNum">' + (r + 1) + '. ' + quiz["questions"][r]["text"] + '</td><td>' + quiz["questions"][r]["answers"][userAnswers[r][2]] + '<td>' + quiz["questions"][r]["answers"][quiz["questions"][r]["correct_answer"]] + '</td></tr>');
+    else
+      $('#scoreTable > tbody:last-child').append('<tr class="danger"><td class="questionNum">' + (r + 1) + '. ' + quiz["questions"][r]["text"] + '</td><td>' + quiz["questions"][r]["answers"][userAnswers[r][2]] + '<td>' + quiz["questions"][r]["answers"][quiz["questions"][r]["correct_answer"]] + '</td></tr>');
+  }
+}
+
+// Create pie chart for score
+function createPieChart(wrong,right,percentW,percentR) {
+  var red = "#FF0000 ";
+  var green = "#006600";
+
+  var chart = document.getElementById('piechart');
+  var ctx = chart.getContext('2d');
+  ctx.clearRect(0, 0, chart.width, chart.height);
+
+  var cx = chart.width/2;
+  var cy = chart.height/2;
+  var radius = 100;
+
+  var wrongFraction = Math.PI * 2.0 * (wrong/(right+wrong));
+  var rightFraction = Math.PI * 2.0 * (right/(right+wrong));
+  console.log(wrong + "  " + wrongFraction);
+  console.log(right + " "  + rightFraction);
+
+  // incorrect
+  ctx.fillStyle = red;
+  ctx.beginPath();
+  ctx.arc(cx, cy, radius, 0, wrongFraction, false);
+  ctx.lineTo(cx,cy);
+  ctx.closePath();
+  ctx.fill();
+
+  // correct
+  ctx.fillStyle = green;
+  ctx.beginPath();
+  ctx.arc(cx, cy, radius, wrongFraction, Math.PI * 2, false);
+  ctx.lineTo(cx,cy);
+  ctx.closePath();
+  ctx.fill();
+
+  // legend
+  ctx.font = "14px Calibri";
+
+  ctx.fillStyle = green;
+  ctx.fillRect(cx + radius + 50,cy - 20,20,15);
+  ctx.fillText("Correct",cx + radius + 80,cy - 10);
+
+  ctx.fillStyle = red;
+  ctx.fillRect(cx + radius + 50,cy + 20,20,15);
+  ctx.fillText("Incorrect",cx + radius + 80,cy + 30);
+}
+
+// Go to next question in quiz
+function nextQuestion() {
+  console.log ("CURRENT QUESTION" +currentQuestion);
+  // Before end of quiz
+  if (currentQuestion<quizLength-1) {
+    // if one of the quiz questions
+    if (currentQuestion > -1) {
+      // if no answer is checked
+      if (!$("input[name='answers']").is(':checked')){
+        $('#answerWarning').fadeIn();
+        if (currentQuestion === 0) {
+          $('#previousQuestion').hide();
+        }
+        else {
+          $('#previousQuestion').show();
+        }
+      }
+      // if an answer is checked
+      else {
+        $('#answerWarning').hide();
+        if (currentQuestion === 0) {
+          $('#previousQuestion').hide();
+        }
+        else {
+          $('#previousQuestion').show();
+        }
+        whichChecked();
+        currentQuestion+=1;
+        generateQA();
+      }
+    }
+    // if before first question of quiz
+    else {
+      $('#previousQuestion').hide();
+      currentQuestion+=1;
+      generateQA();
+    }
+  }
+
+  // End of quiz
+  else {
+    // Check last question of quiz
+    // if answer is not checked
+    if (!$("input[name='answers']").is(':checked')){
+      $('#answerWarning').show();
+    }
+    // if an answer is checked
+    else {
+      $('#answerWarning').hide();
+      whichChecked();// Display end of quiz screen
+      $('#welcome').hide();
+      $('#questionNumber').hide();
+      $('#question').hide();
+      $('#previousQuestion').hide();
+      $('#nextQuestion').hide();
+      $('#answerChoices').hide();
+      $('#piechart').fadeIn("slow");
+      $('#home').show();
+      calculateScore();
+      $('#nameScore').text(name + ", your score on this quiz is: " + score + "/" + quizLength + " questions or " + Math.round(100*score/quizLength) + "%");
+      scorePerQuestionTable();
+      console.log(score);
+      createPieChart(quizLength-score, score, ((quizLength-score)*100)/quizLength, 100*score/quizLength);
+    }
+  }
+}

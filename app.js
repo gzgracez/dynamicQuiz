5937842c733cbd8e826d1cc32558f7ebb3c2e3cf
Grace Zhang <gz.gracez16@gmail.com>
2015-10-20 14:00:56 -0400

    public

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+app.use("/public", express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
   res.send('Hello World!');

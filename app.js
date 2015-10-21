2238e9a4ad0ffb43aaeaba0d5ee684c6b40ea2a1
Grace Z <gz.gracez16@gmail.com>
2015-10-20 22:32:28 -0400

    heroku port

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.get('/', function (req, res) {
 
 });
 
-server.listen(process.env.PORT || 4000, function() {
+var server = app.listen(process.env.PORT || 4000, function() {
   var host = server.address().address;
   var port = server.address().port;
   console.log('Example app listening at http://%s:%s', host, port);
